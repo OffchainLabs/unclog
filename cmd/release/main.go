de7680e69c62bb07364ac3d6f063b80ee58725bc
Kasey Kirkham <kasey@users.noreply.github.com>
2025-01-08 19:34:00 -0600

    overwrite changelog (rather than write to stdout)

--- cmd/release/main.go
+++ cmd/release/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
+	"path"
 	"time"
 
 	"github.com/OffchainLabs/unclog/changelog"
@@ -19,7 +21,11 @@ func parseArgs(args []string) (*changelog.Config, error) {
 	flags.BoolVar(&c.Cleanup, "cleanup", false, "Remove the changelog fragment files after generating the changelog")
 	flags.Parse(args)
 	if c.RepoPath == "" {
-		return c, fmt.Errorf("repo is required")
+		wd, err := os.Getwd()
+		if err != nil {
+			return nil, fmt.Errorf("repo flag not set and can't get working directory from syscall, %w", err)
+		}
+		c.RepoPath = wd
 	}
 	if c.Tag == "" {
 		return c, fmt.Errorf("tag is required")
@@ -39,6 +45,9 @@ func Run(ctx context.Context, args []string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(out)
+	clPath := path.Join(cfg.RepoPath, cfg.PreviousPath)
+	if err := os.WriteFile(clPath, []byte(out), 0644); err != nil {
+		return err
+	}
 	return nil
 }
